ea18e15aec72cc9af5e974af05a2f9bc0a6be0af
Julien Baouz <baouzjulien@gmail.com>
2025-11-15 16:59:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// variables
+const regexPattern = document.getElementById('pattern');
+const stringToTest = document.getElementById('test-string');
+const testButton = document.getElementById('test-btn');
+const testResult = document.getElementById('result');
+const caseInsensitiveFlag = document.getElementById('i');
+const globalFlag = document.getElementById('g'); 
 
+// fonction getFlags
+
+const getFlags = () => {
+let isFlag = "";
+if(caseInsensitiveFlag.checked) {
+  isFlag += "i";
+};
+if(globalFlag.checked) {
+  isFlag += "g";
+};
+return isFlag;
+};
+
+// addEvent Test
+
+testButton.addEventListener('click', () => {
+  let isFlag = getFlags();
+  console.log(`/${regexPattern.value}/${isFlag}`);
+});
